f213afc53ae3fd93d84c70e9ad057dfaac13f582
alrazihi <tawfikalrazihi@gmail.com>
2023-05-21 23:14:06 +0200

    until login

---
+++ app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { HomeComponent } from 'src/app/home/home.component';
+import { ProfileComponent } from 'src/app/profile/profile.component';
+import { WebapiComponent } from 'src/app/webapi/webapi.component';
+
+
+const routes: Routes = [
+  /* Changes start here. */
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    // The profile component is protected with MSAL Guard.
+    canActivate: [MsalGuard]
+  },
+  {
+    path: 'webapi',
+    component: WebapiComponent,
+    // The profile component is protected with MSAL Guard.
+    canActivate: [MsalGuard]
+  },
+  {
+    // The home component allows anonymous access
+    path: '',
+    component: HomeComponent
+  }
+  /* Changes end here. */
+];
+
+
+@NgModule({
+  /* Changes start here. */
+  // Replace the following line with the next one
+  //imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation:'enabledNonBlocking'
+  })],
+  /* Changes end here. */
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
